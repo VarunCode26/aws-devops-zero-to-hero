825f91813ca0905187a15b937d546d585b99b8bf
VarunCode26 <80455201+VarunCode26@users.noreply.github.com>
2025-11-20 19:18:18 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Stop the running container (if any)
-echo "Stopping existing container...."
-id=docker ps | awk -F " " '{print $1}'
-docker rm -f $id
+echo "Deployment done!!"
+#echo "Stopping existing container...."
+#id=docker ps | awk -F " " '{print $1}'
+#docker rm -f $id
